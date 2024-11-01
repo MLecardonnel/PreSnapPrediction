da64c306bc88364ca49a1e29753d1252c4effc36
MaximeLecardonnel6x7 <mlecardo@hotmail.fr>
2024-11-01 23:13:31 +0100

    ORPSP Classification.

---
+++ src/pre_snap_prediction/modeling/orpsp_classification.py
@@ -0,0 +1,165 @@
+import numpy as np
+import polars as pl
+from catboost import CatBoostClassifier
+from pre_snap_prediction.utils.constants import FEATURES, FEATURES_TO_ENCODE
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OrdinalEncoder
+
+
+def select_orpsp_features(features: pl.DataFrame) -> pl.DataFrame:
+    """Selects a specific subset of ORPSP features.
+
+    Parameters
+    ----------
+    features : pl.DataFrame
+        A Polars DataFrame containing the full set of computed ORPSP features.
+
+    Returns
+    -------
+    pl.DataFrame
+        A Polars DataFrame with only the selected ORPSP features.
+    """
+    return features.select(["gameId", "playId", "nflId"] + FEATURES)
+
+
+def train_encoder(data: pl.DataFrame) -> OrdinalEncoder:
+    """Trains an OrdinalEncoder on specified categorical features.
+
+    Parameters
+    ----------
+    data : pl.DataFrame
+        A Polars DataFrame containing the categorical features to be encoded.
+
+    Returns
+    -------
+    OrdinalEncoder
+        A trained encoder ready for transforming the categorical columns into ordinal values.
+    """
+    encoder = OrdinalEncoder().fit(data.select(FEATURES_TO_ENCODE))
+
+    return encoder
+
+
+def transform_encoder(data: pl.DataFrame, encoder: OrdinalEncoder) -> pl.DataFrame:
+    """Transforms specified categorical features in the DataFrame using a fitted OrdinalEncoder.
+
+    Parameters
+    ----------
+    data : pl.DataFrame
+        A Polars DataFrame containing the features to encode.
+    encoder : OrdinalEncoder
+        A trained OrdinalEncoder used to transform the categorical features.
+
+    Returns
+    -------
+    pl.DataFrame
+        A Polars DataFrame identical in structure to the input data, but with encoded categorical features.
+    """
+    data_to_encode = data.select(FEATURES_TO_ENCODE)
+
+    other_data = data.drop(FEATURES_TO_ENCODE)
+    encoded_data = pl.DataFrame(encoder.transform(data_to_encode), schema=data_to_encode.columns)
+    encoded_data = pl.concat([other_data, encoded_data], how="horizontal").select(data.columns)
+
+    return encoded_data
+
+
+def orpsp_train_test_split(
+    encoded_data: pl.DataFrame, target_data: pl.DataFrame, test_size: float = 0.3
+) -> tuple[pl.DataFrame, pl.DataFrame, pl.DataFrame, pl.DataFrame]:
+    """Splits the ORPSP dataset into training and testing sets.
+
+    Parameters
+    ----------
+    encoded_data : pl.DataFrame
+        A Polars DataFrame containing encoded features.
+    target_data : pl.DataFrame
+        A Polars DataFrame containing the ORPSP target variable
+    test_size : float, optional
+        The proportion of the dataset to include in the test split, by default 0.3
+
+    Returns
+    -------
+    tuple[pl.DataFrame, pl.DataFrame, pl.DataFrame, pl.DataFrame]
+        Four DataFrames representing the training and testing splits.
+    """
+    learning_data = encoded_data.join(
+        target_data,
+        on=["gameId", "playId", "nflId"],
+        how="left",
+    )
+    learning_data = learning_data.filter(pl.col("orpsp_target").is_not_null())
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        learning_data.drop("orpsp_target"),
+        learning_data.select("orpsp_target"),
+        test_size=test_size,
+    )
+
+    return x_train, x_test, y_train, y_test
+
+
+def train_orpsp_classification(x_train: pl.DataFrame, y_train: pl.DataFrame, max_depth: int = 6) -> CatBoostClassifier:
+    """Trains a CatBoost classification model to predict ORPSP.
+
+    Parameters
+    ----------
+    x_train : pl.DataFrame
+        A Polars DataFrame containing training features data.
+    y_train : pl.DataFrame
+        A Polars DataFrame containing training labels for the ORPSP classification.
+    max_depth : int, optional
+        Maximum depth of the tree in the CatBoostClassifier, by default 6
+
+    Returns
+    -------
+    CatBoostClassifier
+        A trained CatBoostClassifier model ready for ORPSP prediction.
+    """
+    classification_model = CatBoostClassifier(max_depth=max_depth, learning_rate=0.01).fit(
+        x_train.drop(["gameId", "playId", "nflId"]).to_numpy(),
+        y_train.to_numpy(),
+        verbose=False,
+    )
+
+    return classification_model
+
+
+def predict_orpsp_class(x: pl.DataFrame, classification_model: CatBoostClassifier) -> np.ndarray:
+    """Uses a trained CatBoost classification model to predict ORPSP target classes.
+
+    Parameters
+    ----------
+    x : pl.DataFrame
+        A Polars DataFrame containing features data.
+    classification_model : CatBoostClassifier
+        A trained CatBoostClassifier model ready for ORPSP prediction.
+
+    Returns
+    -------
+    np.ndarray
+        Array of predicted class labels for the ORPSP target.
+    """
+    return classification_model.predict(x.drop(["gameId", "playId", "nflId"]).to_numpy())
+
+
+def predict_orpsp(x: pl.DataFrame, classification_model: CatBoostClassifier) -> pl.DataFrame:
+    """Generates ORPSP probability predictions using a trained CatBoost classification model.
+
+    Parameters
+    ----------
+    x : pl.DataFrame
+        A Polars DataFrame containing features data.
+    classification_model : CatBoostClassifier
+        A trained CatBoostClassifier model ready for ORPSP prediction.
+
+    Returns
+    -------
+    pl.DataFrame
+        A Polars DataFrame containing the predicted ORPSP.
+    """
+    predictions = x.select(["gameId", "playId", "nflId"])
+    predictions = predictions.with_columns(
+        pl.Series("orpsp", classification_model.predict_proba(x.drop(["gameId", "playId", "nflId"]).to_numpy())[:, 1])
+    )
+    return predictions
--- src/pre_snap_prediction/utils/constants.py
+++ src/pre_snap_prediction/utils/constants.py
@@ -16,3 +16,48 @@ ROUTES_CONVERSION = {
     "ANGLE": "wheelangle",
     "WHEEL": "wheelangle",
 }
+
+
+FEATURES = [
+    "gameId",
+    "down",
+    "yardsToGo",
+    "offenseFormation",
+    "receiverAlignment",
+    "playAction",
+    "position",
+    "route_mode",
+    "route_time_mean",
+    "dis_recep_zone",
+    "dir_recep_zone",
+    "recep_zone_dis_out_of_bounds",
+    "recep_zone_dis_yard_line",
+    "dis_out_of_bounds",
+    "dis_back_of_endzone",
+    "dis_yard_line",
+    "nb_early45angle",
+    "nb_flat",
+    "nb_90angle",
+    "nb_late45angle",
+    "nb_shortstraight",
+    "nb_wheelangle",
+    "nb_screen",
+    "nb_straight",
+    "nb_routes",
+    "nb_recep_zone_negative",
+    "nb_recep_zone_5",
+    "nb_recep_zone_10",
+    "nb_recep_zone_20",
+    "nb_recep_zone_inf",
+    "dis_ball_to_recep_zone",
+    "dir_ball_to_recep_zone",
+]
+
+
+FEATURES_TO_ENCODE = [
+    "offenseFormation",
+    "receiverAlignment",
+    "playAction",
+    "route_mode",
+    "position",
+]
